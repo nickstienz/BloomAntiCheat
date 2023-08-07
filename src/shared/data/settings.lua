48c52912ed9c10f3c5a0633c89b456ba82fb987b
Nicholas Stienz <nickstienz@gmail.com>
2023-08-07 13:56:57 -0500

    Better Logging

--- src/shared/data/settings.lua
+++ src/shared/data/settings.lua
@@ -2,8 +2,8 @@ local settings = {
 
 
 --[ Logging ]--
-output_logs = true,          -- Whether or not to output logs to the console (boolean)
-max_log_size = 100            -- The maximum number of recent logs to store at once (number)
+output_logs = true,           -- Whether or not to output logs to the console (boolean)
+max_log_size = 100,           -- The maximum number of recent logs to store at once (number)
 
 
 }
--- src/shared/utils/logging.lua
+++ src/shared/utils/logging.lua
@@ -1,39 +1,51 @@
-local settings = require(game.ReplicatedStorage:WaitForChild("BloomShared").data.settings)
+local logging = {}
+
+local replicated_storage = game:GetService("ReplicatedStorage")
+
+local settings = require(replicated_storage.bloom_shared.data.settings)
 local output_logs = settings.output_logs
 local max_log_size = settings.max_log_size
 
-local logging = {}
-
-local logs = {}
-local log_errors = {}
+local logs_table = {}
+local log_errors_table = {}
 local head = 1
 
-local function log(message: string)
-    logs[head] = message
+local function log(file_name: string, message: string)
+    local formated_message = message .. " (" .. file_name .. ")"
+    logs_table[head] = formated_message
+
     head = (head % max_log_size) + 1
 
-    if output_logs then print("(Bloom)~" .. message) end
+    if output_logs then print("(Bloom)~" .. formated_message) end
 end
 
-function logging.okay(message: string)
-    log("[+] " .. message)
+function logging.okay(file_name: string, message: string)
+    log(file_name, "[+] " .. message)
 end
 
-function logging.info(message: string)
-    log("[i] " .. message)
+function logging.info(file_name: string, message: string)
+    log(file_name, "[i] " .. message)
 end
 
-function logging.warn(message: string)
-    log("[!] " .. message)
+function logging.warn(file_name: string, message: string)
+    log(file_name, "[!] " .. message)
 end
 
-function logging.error(message: string)
-    log("[-] " .. message)
-    table.insert(log_errors, message)
+function logging.error(file_name: string, message: string)
+    log(file_name, "[-] " .. message)
+
+    local formated_message = "Error: " .. message .. " (" .. file_name .. ")"
+    if #log_errors_table < 999 then
+        table.insert(log_errors_table, formated_message)
+    end
 end
 
 function logging.get_logs()
-    return logs
+    return logs_table
+end
+
+function logging.get_errors()
+    return log_errors_table
 end
 
 return logging
