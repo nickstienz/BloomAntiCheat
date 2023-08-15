5dc90c3226d737725312366edc0290f59384e51a
Nicholas Stienz <nickstienz@gmail.com>
2023-08-15 12:11:31 -0500

    Better Logging

---
+++ src/shared/settings.lua
@@ -0,0 +1,10 @@
+return {
+
+
+--[ Logging ]--
+do_logging = true,              -- Enable and disable logging (boolean)
+print_logs = 3,                 -- 0 = No print, 1 = Only Studios, 2 = Only Game, 3 = All (number)
+max_log_size = 100,             -- The maximum number of recent logs to store at once (number)
+
+
+}
--- src/shared/utils/logging.lua
+++ src/shared/utils/logging.lua
@@ -1,22 +1,35 @@
 local logging = {}
 
 local replicated_storage = game:GetService("ReplicatedStorage")
+local run_service = game:GetService("RunService")
 
-local settings = require(replicated_storage.bloom_shared.data.settings)
-local output_logs = settings.output_logs
+local settings = require(replicated_storage.bloom_shared.settings)
+local do_logging = settings.do_logging
+local print_logs = settings.output_logs
 local max_log_size = settings.max_log_size
+
+local is_studio = run_service:IsStudio()
 
 local logs_table = {}
 local log_errors_table = {}
 local head = 1
 
 local function log(file_name: string, message: string)
+    if not do_logging then return end
+
     local formated_message = message .. " (" .. file_name .. ")"
     logs_table[head] = formated_message
-
     head = (head % max_log_size) + 1
 
-    if output_logs then print("(Bloom)~" .. formated_message) end
+    if print_logs ~= 0 then -- No prints
+        if print_logs == 3 then -- All
+            print("(Bloom)~" .. formated_message)
+        elseif print_logs == 2 and not is_studio then -- Only Game
+            print("(Bloom)~" .. formated_message)
+        elseif print_logs == 1 and is_studio then -- Only Studio
+            print("(Bloom)~" .. formated_message)
+        end
+    end
 end
 
 function logging.okay(file_name: string, message: string)
@@ -28,11 +41,13 @@ function logging.info(file_name: string, message: string)
 end
 
 function logging.warn(file_name: string, message: string)
-    log(file_name, "[!] " .. message)
+    log(file_name, "[-] " .. message)
 end
 
 function logging.error(file_name: string, message: string)
-    log(file_name, "[-] " .. message)
+    log(file_name, "[!] " .. message)
+
+    if not do_logging then return end
 
     local formated_message = "Error: " .. message .. " (" .. file_name .. ")"
     if #log_errors_table < 999 then
